a9db5728de1d7907004c660cc2b58e379ba5b035
thinzarwinluck <thinzarwinluck0909@gmail.com>
2023-10-26 20:25:19 +0630

    feat:custom login and permission denied

--- kyn/src/main/java/com/assignment3/kyn_project/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment3.kyn_project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role , Long>{
-	
-}
